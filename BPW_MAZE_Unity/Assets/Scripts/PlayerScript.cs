4b80402331a86b662b2e0b83a3a574a2e5354714
Watenk <Ivar.nuij@gmail.com>
2022-05-31 13:07:49 +0200

    Added FlashLight

--- BPW_MAZE_Unity/Assets/Scripts/PlayerScript.cs
+++ BPW_MAZE_Unity/Assets/Scripts/PlayerScript.cs
@@ -7,24 +7,36 @@ public class PlayerScript : MonoBehaviour
 {
     //FindObject
     private MazeGenerator.MazeGen mazeGenerator;
+    private FindFlashlight flashlight;
 
-    public bool canMove = false;
+    //PLayerMovement
+    public float timeToMove = 0.5f;
 
+    private bool canMove = true;
     private bool isMoving = false;
     private Vector3 origPos, targetPos;
     private Vector3 up, left, down, right;
-    private float timeToMove = 0.2f;
+
+    //FlashlightMovement
+    private Vector3 currentRotation = new Vector3 (0, 90, 90);
+    private Vector3 rightRotation = new Vector3(0, 90, 90), downRotation = new Vector3(90, 90, 90), LeftRotation = new Vector3(180, 90, 90), upRotation = new Vector3(270, 90, 90);
+
+    //NextTurn Event
+    public delegate void NextTurn();
+    public event NextTurn nextTurn;
 
     private void Awake()
     {
         //FindObject
         mazeGenerator = FindObjectOfType<MazeGenerator.MazeGen>();
+        flashlight = FindObjectOfType<FindFlashlight>();
     }
 
     void Update()
     {
         if (!isMoving && canMove == true)
         {
+            //Player Movement
             if (Input.GetKey(KeyCode.W))
             {
                 StartCoroutine(MovePlayer(up = new Vector3(0, 1f, 0)));
@@ -44,12 +56,36 @@ public class PlayerScript : MonoBehaviour
             {
                 StartCoroutine(MovePlayer(right = new Vector3(1f, 0, 0)));
             }
+
+            //Player FlashLight
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                StartCoroutine(MoveFlashLight(rightRotation));
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                StartCoroutine(MoveFlashLight(downRotation));
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                StartCoroutine(MoveFlashLight(LeftRotation));
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                StartCoroutine(MoveFlashLight(upRotation));
+            }
         }
     }
 
+    //MovePlayer
     private IEnumerator MovePlayer(Vector3 direction)
     {
+        //Start Moving
         isMoving = true;
+        canMove = false;
 
         float elapsedTime = 0;
         origPos = transform.position;
@@ -61,16 +97,50 @@ public class PlayerScript : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
         transform.position = targetPos;
 
         isMoving = false;
+        //Moving done
+        //nextTurn Event
+        if (nextTurn != null)
+        {
+            nextTurn();
+        }
+        
+        canMove = true;
+    }
+
+    //MoveFlashlight
+    private IEnumerator MoveFlashLight(Vector3 targetRotation)
+    {
+        //Start Moving
+        isMoving = true;
         canMove = false;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < timeToMove)
+        {
+            flashlight.transform.rotation = Quaternion.Euler(Vector3.Lerp(currentRotation, targetRotation, (elapsedTime / timeToMove)));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        flashlight.transform.rotation = Quaternion.Euler(targetRotation.x, 90, 90);
+        currentRotation = new Vector3(targetRotation.x, 90, 90);
+
+        isMoving = false;
+        canMove = true;
     }
 
     public void SpawnPlayer()
     {
         Vector3Int spawnLocation = mazeGenerator.roomList[0].GetCenter();
         gameObject.transform.position = new Vector3Int(spawnLocation.x, spawnLocation.y, -1);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        targetPos = origPos;
     }
 }
